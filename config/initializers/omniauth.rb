a0804db1a63a6bf2d2eeb26c80d0d16115debf93
Ian Connor <iconnor@projectlounge.com>
2013-06-02 15:33:33 +1000

    Remove test auth

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '621694244508698', 'f0e01d613935fa93763b9c7be946a7c2'
-  provider :google, '100215991152.apps.googleusercontent.com', '411IEq90_TIs-HO8HPnHxgW-'
-
-end
